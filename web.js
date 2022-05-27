8148b5fd32783d6a31ac6eacae1a0da95ecb506a
Moses Yeung <yoyeung@gmail.com>
2022-05-27 13:00:01 +0800

    update

--- web.js
+++ web.js
@@ -34,27 +34,8 @@ app.post('/', function (req, res) {
 function shotOrGo(me,closeToMe) {
     if (closeToMe[0] > closeToMe[1]) {
         // move to close one like Y
-        if (closeToMe[0] >= 1) { //3
-            if (me.direction === 'S' && Math.abs(closeToMe[0]) < 3) {
-                return 'T'
-            } else if(me.direction ==='E') {
-                return 'R'
-            } else if (me.direction ==='N' || me.direction ==='W') {
-                return 'L'
-            }
-        } else { //4
-            if (me.direction === 'N' && Math.abs(closeToMe[0]) < 3) {
-                return 'T'
-            } else if(me.direction ==='E') {
-                return 'L'
-            } else if (me.direction ==='S' || me.direction ==='W') {
-                return 'R'
-            }
-        }
-        
-    } else {
-        if (closeToMe[1] >= 1) { //1
-            if (me.direction === 'W' && Math.abs(closeToMe[1]) < 3) {
+        if (closeToMe[0] >= 1) { //1
+            if (me.direction === 'W' && Math.abs(closeToMe[0]) < 3) {
                 return 'T'
             } else if(me.direction ==='S') {
                 return 'R'
@@ -62,12 +43,31 @@ function shotOrGo(me,closeToMe) {
                 return 'L'
             }
         } else { //2
-            if (me.direction === 'E' && Math.abs(closeToMe[1]) < 3) {
+            if (me.direction === 'E' && Math.abs(closeToMe[0]) < 3) {
                 return 'T'
             } else if(me.direction ==='S') {
                 return 'R'
             } else if (me.direction ==='N' || me.direction ==='W') {
                 return 'L'
+            }
+        }
+        
+    } else {
+        if (closeToMe[1] >= 1) { //3
+            if (me.direction === 'S' && Math.abs(closeToMe[1]) < 3) {
+                return 'T'
+            } else if(me.direction ==='E') {
+                return 'R'
+            } else if (me.direction ==='N' || me.direction ==='W') {
+                return 'L'
+            }
+        } else { //4
+            if (me.direction === 'N' && Math.abs(closeToMe[1]) < 3) {
+                return 'T'
+            } else if(me.direction ==='E') {
+                return 'L'
+            } else if (me.direction ==='S' || me.direction ==='W') {
+                return 'R'
             }
         }
     }
