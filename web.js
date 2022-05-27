5f3a6c01bd43af2c3daae4a83d2d5ea6925e34d4
Moses Yeung <yo_yeung@yahoo.com.hk>
2022-05-27 11:57:21 +0800

    Update web.js

--- web.js
+++ web.js
@@ -1,6 +1,10 @@
+
+const URL = "https://cloud-run-hackathon-nodejs-zzovtf46pq-uc.a.run.app"
+
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const { match } = require('assert');
 
 app.use(bodyParser.json());
 
@@ -9,9 +13,62 @@ app.get('/', function (req, res) {
 });
 
 app.post('/', function (req, res) {
+    const moves = ['F', 'R', 'L', 'T'];
   console.log(JSON.stringify(req.body));
-  const moves = ['F', 'R', 'L', 'T'];
-  res.send(moves[Math.floor(Math.random() * moves.length)]);
+  let closeToMe = [100,100]
+  const  me = req.body.arena.state[URL]
+  const players = Object.values(req.body.arena.state)
+  players.forEach(player => {
+      const possibleX = me.x - player.x
+      const possibleY = me.y - player.y
+      if (match.abs(possibleX) < match.abs(closeToMe[0])) {
+        closeToMe[0] = possibleX
+      }
+      if (match.abs(possibleY) < match.abs(closeToMe[1])) {
+        closeToMe[1] = possibleY
+      }
+  })
+  
+  res.send(shotOrGo(me, closeToMe));
 });
+
+function shotOrGo(me,closeToMe) {
+    if (closeToMe[0] > closeToMe[1]) {
+        // move to close one like Y
+        if (closeToMe[1] === 1) {
+            if (me.direction === 'W') {
+                return 'T'
+            } else if(me.direction ==='E') {
+                return 'R'
+            }
+            return 'L'
+        } else {
+            if (me.direction === 'N') {
+                return 'T'
+            } else if(me.direction ==='E') {
+                return 'L'
+            }
+            return 'R'
+        }
+        
+    } else {
+        if (closeToMe[0] === 1) {
+            if (me.direction === 'W') {
+                return 'T'
+            } else if(me.direction ==='E') {
+                return 'R'
+            }
+            return 'L'
+        } else {
+            if (me.direction === 'N') {
+                return 'T'
+            } else if(me.direction ==='E') {
+                return 'L'
+            }
+            return 'R'
+        }
+    }
+    
+}
 
 app.listen(process.env.PORT || 8080);
