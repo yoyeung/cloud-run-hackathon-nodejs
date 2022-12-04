0b4cfb5ceadd76e78d43939be839db0c215c35c9
Moses Yeung <yo_yeung@yahoo.com.hk>
2022-12-04 19:13:52 +0800

    clean up

--- web.js
+++ web.js
@@ -12,55 +12,8 @@ var isShot = true
 var last = 0
 var stack = []
 const maxDistinct = 2
+var hitCount = 0
 
-// app.post('/', function (req, res) {
-//     const moves = ['R', 'L','R', 'R','R', 'R','L', 'L','R', 'L'];
-  
-//   const URL = `${req.protocol}://${req.hostname}/`
-//   console.log(JSON.stringify(req.body));
-//   let closeToMe = [100,100]
-//   let score = 100
-//   const  me = req.body.arena.state[URL]
-//   delete req.body.arena.state[URL]
-//   const players = Object.values(req.body.arena.state)
-//   console.log(players)
-//   players.forEach(player => {
-//       const possibleX = me.x - player.x
-//       const possibleY = me.y - player.y
-//       if ((Math.abs(possibleX) + Math.abs(possibleY)) < score) {
-//         closeToMe[0] = possibleX
-//         closeToMe[1] = possibleY
-//         score = (Math.abs(possibleX) + Math.abs(possibleY))
-//       }
-//   })
-//   if (me.wasHit && isShot === false) {
-//     isShot = true
-//   }
-//   if (isShot && last <=2) {
-//       if (last === 0){
-//         last += 1
-//         res.send(moves[Math.floor(Math.random() * moves.length)])
-//       }
-        
-//       if (last <= 3){
-//         last += 1
-//         res.send('F')
-//       }
-//       last += 1
-//       if (last >=4){
-//         isShot = false 
-//         last = 0
-//       }
-      
-//   }
-//   console.log('result', shotOrGo(me, closeToMe), closeToMe, me)
-//   stack.push(shotOrGo(me, closeToMe))
-//   if (stack.length > 50) {
-//     stack = []
-//     res.send(moves[Math.floor(Math.random() * moves.length)])
-//   }
-//   res.send(shotOrGo(me, closeToMe));
-// });
 
 app.post('/', function (req, res) {
     const moves = ['R', 'L','R', 'R','F', 'R','L', 'F','R', 'L'];
@@ -81,11 +34,6 @@ app.post('/', function (req, res) {
     return res.send('T')
   }
   actionToTake(me, filteredPlayers, res)
-  
-  
-  
-  
-//   res.send(moves[Math.floor(Math.random() * moves.length)])
   
 });
 
@@ -191,46 +139,7 @@ function actionToTake(me, players, res) {
   }
    return res.send('T')
 }
-// function checkCloseToMe(me){
-//  return (player) => {
-//     if (me.direction === 'N') {
-//       if(player.y < me.y) {
-//         if (me.y - player.y < 3) {
-//           return res.send('T')
-//         } else {
-//           return res.send('F')
-//         }
-//       }
-      
-//     } else if (me.direction === 'E') {
-//       if(player.x > me.x) {
-//         if (player.x - me.x < 3) {
-//           return res.send('T')
-//         } else {
-//           return res.send('F')
-//         }
-//       }
-      
-//     } else if (me.direction === 'W') {
-//       if(player.x < me.x) {
-//         if (me.x - player.x < 3) {
-//           return res.send('T')
-//         } else {
-//           return res.send('F')
-//         }
-//       }
-//     } else {
-//       if(player.y > me.y) {
-//         if (player.y - me.y < 3) {
-//           return res.send('T')
-//         } else {
-//           return res.send('F')
-//         }
-//       }
-//     }
-    
-//  }
-// }
+
 
 function filterForSameRow(me){ 
   return (player) => {
@@ -241,50 +150,5 @@ function filterForSameRow(me){
   }
 }
 
-
-// function shotOrGo(me,closeToMe) {
-//     const moves2 = ['R','F', 'F','F','F', 'L','F'];
-//     if (closeToMe[0] > closeToMe[1]) {
-//         if (closeToMe[0] > 0) { //1
-//             if (me.direction === 'W' && Math.abs(closeToMe[0]) < 3  &&  Math.abs(closeToMe[1]) === 0) {
-//                 return 'T'
-//             } else if(me.direction ==='S') {
-//                 return 'R'
-//             } else if (me.direction ==='N' || me.direction ==='E') {
-//                 return 'L'
-//             }
-//         } else { //2
-//             if (me.direction === 'E' && Math.abs(closeToMe[0]) < 3  &&  Math.abs(closeToMe[1]) === 0) {
-//                 return 'T'
-//             } else if(me.direction ==='S') {
-//                 return 'L'
-//             } else if (me.direction ==='N' || me.direction ==='W') {
-//                 return 'R'
-//             }
-//         }
-        
-//     } else {
-//         if (closeToMe[1] > 0) { //3
-//             if (me.direction === 'N' && Math.abs(closeToMe[1]) < 3 &&  Math.abs(closeToMe[0]) === 0) {
-//                 return 'T'
-//             } else if(me.direction ==='E') {
-//                 return 'L'
-//             } else if (me.direction ==='S' || me.direction ==='W') {
-//                 return 'R'
-//             }
-//         } else { //4
-//             if (me.direction === 'S' && Math.abs(closeToMe[1]) < 3  &&  Math.abs(closeToMe[0]) === 0) {
-//                 return 'T'
-//             } else if(me.direction ==='E') {
-//                 return 'R'
-//             } else if (me.direction ==='N' || me.direction ==='W') {
-//                 return 'L'
-//             }
-//         }
-//     }
-
-//     return moves2[Math.floor(Math.random() * moves2.length)]
-    
-// }
 
 app.listen(process.env.PORT || 8080);
